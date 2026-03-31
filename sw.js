67bdd629ded476f2967d657fa1d3cb2ce7f9b5e6
nbrfr55557-jpg <pierrick.stm@gmail.com>
2026-03-31 09:20:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
-const CACHE = 'peaklog-v1.2.2';
+const CACHE = 'peaklog-v1.2.3';
 const FILES = [
   '/PEAKLOG/index.html',
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Mono:wght@400;500&family=DM+Sans:wght@300;400;500&display=swap'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
@@ -23,10 +19,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
       if (e.request.url.includes('fonts.googleapis.com') || e.request.url.includes('fonts.gstatic.com')) {
-        return caches.open(CACHE).then(c => {
-          c.put(e.request, res.clone());
-          return res;
-        });
+        return caches.open(CACHE).then(c => { c.put(e.request, res.clone()); return res; });
       }
       return res;
     }))
@@ -34,7 +27,5 @@ self.addEventListener('fetch', e => {
 });
 
 self.addEventListener('message', e => {
-  if (e.data?.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
+  if (e.data?.action === 'skipWaiting') self.skipWaiting();
 });
